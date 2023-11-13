bcca7608092da572a1696a0728163f1e8673a632
Fred <fengyiyi@users.noreply.github.com>
2023-11-13 13:17:20 +0800

    brand

--- app.config.ts
+++ app.config.ts
@@ -1,6 +1,6 @@
 export default defineAppConfig({
   alpine: {
-    title: 'Alpine',
+    title: ' 冯熠熠 品牌设计师',
     description: 'The minimalist blog theme',
     image: {
       src: '/social-card-preview.png',
@@ -9,7 +9,7 @@ export default defineAppConfig({
       height: 300
     },
     header: {
-      position: 'right', // possible value are : | 'left' | 'center' | 'right'
+      position: 'left', // possible value are : | 'left' | 'center' | 'right'
       logo: {
         path: '/logo.svg', // path of the logo
         pathDark: '/logo-dark.svg', // path of the logo in dark mode, leave this empty if you want to use the same logo
